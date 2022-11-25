4972740982f8681c32b3e74f07fb258f87ec13bf
FilipRusiecki <57904846+FilipRusiecki@users.noreply.github.com>
2022-11-25 12:02:01 +0000

    template

---
+++ Gobblet/Gobblet/Game.h
@@ -0,0 +1,26 @@
+
+#ifndef GAME_HPP
+#define GAME_HPP
+#include <SFML/Graphics.hpp>
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	void run();
+
+private:
+	void processEvents();
+	void processKeys(sf::Event t_event);
+
+	void update(sf::Time t_deltaTime);
+	void render();
+
+	sf::RenderWindow m_window; // main SFML window
+	bool m_exitGame; // control exiting game
+
+};
+
+#endif 
+
